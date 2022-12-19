91978d045559d611403947a053f50b4f0e2dd217
Kseniia <rydaya97@gmail.com>
2022-12-19 15:40:11 +0200

    create task manager

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const About = () => (
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Best React app!</h1>
+      <p class="lead">
+        Version <strong>1.0.55</strong>
+      </p>
+    </div>
+  </div>
+);
